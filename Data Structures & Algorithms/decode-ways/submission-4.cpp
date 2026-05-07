3d10eb9d7bfd31ddbf8f5878a55572618935d974
Anirban Chakrabarti <80969389+anirban2crypto@users.noreply.github.com>
2026-05-07 18:39:18 +0530

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    int numDecodings(std::string in_str){
+        int len=in_str.size();
+        if ( len == 0 || in_str[0] == '0' ) return 0;
+        if ( len == 1 ) return 1;
+        // ways[i] represents the number of ways to decode a string of length i
+        //std::vector<int> ways(len+1,0);
+        //ways[0] = 1; // Base case: empty string
+        //ways[1] = 1; // Base case: first char
+        int prev2 = 1;
+        int prev = 1;
+        int curr = 0;
+        for(int i=2; i<=len; i++){
+            curr = 0; // Reset for the current positio
+            // Option 1: Single digit (must not be '0')
+            if (in_str[i-1] != '0'){
+                //ways[i] = ways[i-1];
+                curr=prev;
+            }
+            // Option 2: Two digits (must be between 10 and 26)	
+            std::string two_digits=in_str.substr(i-2, 2);
+            int combined = std::stoi(two_digits);			
+            if (combined <= 26 && combined >= 10) {				
+                    //ways[i] +=  ways[i-2];
+                    curr+=prev2;
+            }    
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;  
+        
+    }
+};
