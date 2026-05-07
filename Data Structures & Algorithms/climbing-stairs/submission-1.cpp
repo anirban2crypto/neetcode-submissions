56b71d27ce491bebc1cf3d4cca083a7aa7d750b6
Anirban Chakrabarti <80969389+anirban2crypto@users.noreply.github.com>
2026-05-07 07:16:03 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    	if (n <= 1){
+            return 1;
+        }        
+        std::vector<int> comb(n+1, 0);
+        comb[0] = 1;
+        comb[1] = 1;
+        for(int i=2; i<= n; i++){
+            comb[i] = comb[i-1] + comb[i-2]; 
+        }
+        return comb[n];
+        
+    }
+};
